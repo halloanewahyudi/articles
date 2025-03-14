fe004c42c869d27e92b2b332467795a11cda6db1
halloanewahyudi <30697981+halloanewahyudi@users.noreply.github.com>
2025-03-14 09:47:17 +0700

    title

--- components/PostList.vue
+++ components/PostList.vue
@@ -66,7 +66,7 @@ const searchPosts = () => {
 
 <template>
   <div class="max-w-3xl mx-auto p-4">
-    <h4 class="text-2xl font-bold mb-4 text-center text-neutral-500">Temukan solusi kendala {{ categorySlug }} anda</h4>
+    <h4 class="text-2xl font-bold mb-4 text-center text-neutral-500">Temukan solusi kendala email anda</h4>
 
     <!-- Search Bar -->
      <div class="relative w-full">
